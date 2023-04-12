26244f9ad66b90a403943fbc54c4f4c4ba2a063a
suy0ng <yunsuyong739@naver.com>
2023-04-12 17:01:29 +0900

    edit header 리스트 경로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
       <ul className="header_list">
         <li><Link to="/">소개글</Link></li>
         <li><Link to="/weather">날씨</Link></li>
-        <li><Link to="/jeju-tour/list">관광지</Link></li>
+        <li><Link to="/jeju-tour/list/1">관광지</Link></li>
       </ul>
     </div>
   );
