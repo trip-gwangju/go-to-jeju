2a7e6feef1b6bba60731cfe20de296c29fc7be10
suy0ng <yunsuyong739@naver.com>
2023-04-12 17:24:50 +0900

    feature TourList

---
+++ src/components/TourListItem.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "../styles/listItem.css";
+
+function TourListItem({ tourItem }) {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    setTags(tourItem.alltag.split(","));
+  }, []);
+
+  console.log(tourItem)
+
+  return (
+    <div className="tour-items" onClick={() => window.location.href=`/jeju-tour/detail/${tourItem?.contentsid}`}>
+      <img
+        src={tourItem?.repPhoto?.photoid?.imgpath}
+        alt="관광지 이미지"
+        className="thumb-img"
+      />
+      <div className="item-info">
+        <h2 className="title">{tourItem?.title}</h2>
+        <p className="address">{tourItem?.address}</p>
+        <p className="tour-tag">
+          #{tags?.[0]} #{tags?.[1]} #{tags?.[2]}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TourListItem;
--- src/pages/TourList.jsx
+++ src/pages/TourList.jsx
@@ -5,24 +5,21 @@ import axios from "axios";
 import { AiOutlineCaretLeft, AiOutlineCaretRight } from "react-icons/ai";
 import TourListItem from "../components/TourListItem";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 function TourList() {
   const { pageNum } = useParams();
   const [tourItems, setTourItem] = useState([]);
   const visitJejuApiKey = process.env.REACT_APP_VISIT_JEJU_API_KEY;
+  const URL = `https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=${visitJejuApiKey}&locale=kr&category=c1`;
   const currentItems = tourItems.slice((pageNum - 1) * 6, pageNum * 6);
 
   const fetchData = async () => {
-    await axios
-      .get(
-        `https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=${visitJejuApiKey}&locale=kr&category=c1`
-      )
-      .then((res) => {
-        setTourItem(res.data.items);
-      });
+    await axios.get(URL).then((res) => {
+      setTourItem(res.data.items);
+    });
   };
 
-  console.log(tourItems);
   useEffect(() => {
     fetchData();
   }, []);
@@ -30,16 +27,29 @@ function TourList() {
   return (
     <div className="tour-list-page">
       <div className="search">
-        <SearchBar />
+        <h1>JEJU 관광지 LIST</h1>
+        {/* <SearchBar /> */}
       </div>
       <div className="content">
-        <AiOutlineCaretLeft size={44} />
+        {pageNum === "1" ? null : (
+          <Link to={`/jeju-tour/list/${pageNum - 1}`}>
+            <AiOutlineCaretLeft size={44} />
+          </Link>
+        )}
         <div className="tour-content">
           {currentItems.map((tourItem) => {
-            return <TourListItem key={tourItem.contentsid} tourItem={tourItem} className='tour-item' />;
+            return (
+              <TourListItem
+                key={tourItem.contentsid}
+                tourItem={tourItem}
+                className="tour-item"
+              />
+            );
           })}
         </div>
-        <AiOutlineCaretRight size={44} />
+        <Link to={`/jeju-tour/list/${pageNum - 1 + 2}`}>
+          <AiOutlineCaretRight size={44} />
+        </Link>
       </div>
     </div>
   );
